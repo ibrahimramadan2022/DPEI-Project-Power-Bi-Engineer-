e6b30204a29df055de5c8af885b810be1d38f64e
Ibrahim Ramadan Ibrahim Kamel <134325261+ibrahimramadan2022@users.noreply.github.com>
2024-11-30 08:20:15 +0200

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,13 @@
+/*1.	Display (Using Union Function)
+a.	 The name and the gender of the dependence that's gender is Female and depending on Female Employee.
+b.	 And the male dependence that depends on Male Employee.
+2.	For each project, list the project name and the total hours per week (for all employees) spent on that project.
+3.	Display the data of the department which has the smallest employee ID over all employees' ID.
+4.	For each department, retrieve the department name and the maximum, minimum and average salary of its employees.
+5.	List the full name of all managers who have no dependents.
+6.	For each department-- if its average salary is less than the average salary of all employees-- display its number, name and number of its employees.
+7.	Retrieve a list of employees names and the projects names they are working on ordered by department number and within each department, ordered alphabetically by last name, first name.
+8.	Try to get the max 2 salaries using subquery
+9.	Get the full name of employees that is similar to any dependent name
+10.	Display the employee number and name if at least one of them have dependents (use exists keyword) self-study.
+*/
